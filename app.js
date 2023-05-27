c37502db7a395f20bd1c08b1f217765b013d9469
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-27 09:48:05 +0530

    Update app.js
    addn of js date format

--- app.js
+++ app.js
@@ -3,35 +3,31 @@ const bodyparser = require('body-parser');
 
 const app = express();
 
+var items = ["buy food","Cook food","Eat food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.get('/',function(req,res){
    var today = new Date(); 
-   var currentdate = today.getDate();
-   var day = '';
-   if(today.getDate()===0){
-    day = "sUNDAY";
-   }
-   else if(today.getDate()===1){
-    day = "MONDAY";
-   }
-   else if(today.getDate()===2){
-    day = "TUESDAY";
-   }
-   else if(today.getDate()===3){
-    day = "WEDNESDAY";
-   }
-   else if(today.getDate()===4){
-    day = "THUSDAY";
-   }
-   else if(today.getDate()===5){
-    day = "FRIDAY";
-   }
-   else{
-    day = "SATURDAY";
-   }
-   res.render('list.ejs',{kindofday: day});
+   
+var options = {
+    weekday: "long",
+    day: 'numeric',
+    month: "long"
+};
 
+var day = today.toLocaleDateString("hi-EN",options);
+   res.render('list.ejs',{kindofday: day, newlistItems: items});
+
+});
+
+app.post('/',function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+    
 });
 
 app.listen(3000, function(){

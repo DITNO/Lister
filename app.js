92a5de657495aada87d8bd63b0836f114f68d6a7
DITNO <92884446+DITNO@users.noreply.github.com>
2023-06-05 23:41:12 +0530

    Update app.js
    addition of work lister

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-//app
+const express = require("express");
+const bodyparser = require("body-parser");
+
 const app = express();
 
-var items = ["buy food","Cook food","Eat food"];
+let items = ["buy food", "Cook food", "Eat food"];
+let workItem = [];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get('/',function(req,res){
-   var today = new Date(); 
-   
-var options = {
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
     weekday: "long",
-    day: 'numeric',
-    month: "long"
-};
+    day: "numeric",
+    month: "long",
+  };
 
-var day = today.toLocaleDateString("hi-EN",options);
-   res.render('list.ejs',{kindofday: day, newlistItems: items});
-
+  let day = today.toLocaleDateString("hi-EN", options);
+  res.render("list.ejs", { listTittle: day, newlistItems: items });
 });
 
-app.post('/',function(req,res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
-    
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItem.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, function(){
-    console.log('server is running on the 3000 server');
+app.get("/work", function (req, res) {
+  res.render("list", { listTittle: "Work List", newlistItems: workItem });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItem.push(item);
+  res.redirect("/work");
+});
+app.listen(3000, function () {
+  console.log("server is running on the 3000 server");
 });
